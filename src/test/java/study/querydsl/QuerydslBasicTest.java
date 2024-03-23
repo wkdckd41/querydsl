9ab90b1bf9927a55b1b12af84b3626e556d889f1
wkdckd41 <wkdckd41@gmail.com>
2024-03-23 16:10:20 +0900

    기본문법 - 검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -47,7 +47,7 @@ public class QuerydslBasicTest {
         // member1을 찾아라.
         String qlString =
                 "select m from Member m" +
-                " where m.username = :username";
+                        " where m.username = :username";
 
         Member findMember = em.createQuery(qlString, Member.class)
                 .setParameter("username", "member1") // 파라미터 바인딩 처리
@@ -65,6 +65,50 @@ public class QuerydslBasicTest {
                 .select(member)
                 .from(member)
                 .where(member.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.between(10, 30)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+        member.username.eq("member1"); // username = 'member1'
+        member.username.ne("member1"); // username != 'member1'
+        member.username.eq("member1").not(); // username != 'member1'
+
+        member.username.isNotNull(); // 이름이 is not null
+        member.username.isNull(); // 이름이 is null
+
+        member.age.in(10, 20); // age in (10, 20)
+        member.age.notIn(10, 20); // age not in (10, 20)
+        member.age.between(10, 30); // between 10, 30
+
+        member.age.goe(30); // age >= 30
+        member.age.gt(30); // age > 30
+        member.age.loe(30); // age <= 30
+        member.age.lt(30); // age < 30
+
+        member.username.like("member%"); // like 검색
+        member.username.contains("member"); // like ‘%member%’ 검색
+        member.username.startsWith("member"); // like ‘member%’ 검색
+    }
+
+    @Test
+    void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
